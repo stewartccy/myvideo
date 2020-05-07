48e27ba9ea1bad8a2b09c5ed4a5c3546eb9ed342
453928448@qq.com <sz951016>
2020-05-07 22:39:11 +0800

    spring初始项目

---
+++ src/main/java/com/nullpointer/myvideo/MyvideoApplication.java
@@ -0,0 +1,13 @@
+package com.nullpointer.myvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyvideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyvideoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nullpointer/myvideo/MyvideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nullpointer.myvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyvideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
